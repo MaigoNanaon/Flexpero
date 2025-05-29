f7413cdf25aa569b0483df41dabde01615c7ea0d
unknown <nanaonmaigo@gmail.com>
2025-05-29 22:15:21 +0800

    ab

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+# 创建 Flask 应用
+app = Flask(__name__, template_folder='.')
+
+# 定义路由，渲染 templates/indexb.html 文件
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # 运行 Flask 应用
+    app.run(host='0.0.0.0', port=8500, debug=True)
